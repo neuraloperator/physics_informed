461195053fe865e2ecc6907dd71fd36ce4632ca9
devzhk <devzhk@gmail.com>
2021-08-10 14:35:44 -0700

    Add experiments

--- scripts/addon-2.sh
+++ scripts/addon-2.sh
@@ -2,6 +2,6 @@
 CUDA_VISIBLE_DEVICES=0 python3 run_pino3d.py \
 --config_path configs/transfer/Re400to100-1s.yaml \
 --start 0 \
---stop 80 \
+--stop 40 \
 --log;
 
--- scripts/addon-3.sh
+++ scripts/addon-3.sh
@@ -2,8 +2,14 @@
 CUDA_VISIBLE_DEVICES=0 python3 run_pino3d.py \
 --config_path configs/transfer/Re400to500-1s.yaml \
 --start 0 \
---stop 80 \
+--stop 40 \
 --log;
+CUDA_VISIBLE_DEVICES=0 python3 run_pino3d.py \
+--config_path configs/transfer/Re250to500-1s.yaml \
+--start 33 \
+--stop 40 \
+--log;
+
 
 
 
