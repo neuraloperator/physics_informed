2d919b171ebab11c62178e500c04d14ee41893ed
devzhk <hzzheng@caltech.edu>
2021-10-05 13:14:12 -0700

    Train and test for Burgers

--- train_utils/eval_2d.py
+++ train_utils/eval_2d.py
@@ -3,7 +3,7 @@ import numpy as np
 
 import torch
 
-from .losses import LpLoss, darcy_loss
+from .losses import LpLoss, darcy_loss, PINO_loss
 
 try:
     import wandb
@@ -49,10 +49,45 @@ def eval_darcy(model,
                     )
                 )
     mean_f_err = np.mean(f_val)
-    std_f_err  = np.std(f_val, ddof=1) / np.sqrt(len(f_val))
+    std_f_err = np.std(f_val, ddof=1) / np.sqrt(len(f_val))
 
     mean_err = np.mean(test_err)
     std_err = np.std(test_err, ddof=1) / np.sqrt(len(test_err))
 
     print(f'==Averaged relative L2 error mean: {mean_err}, std error: {std_err}==\n'
           f'==Averaged equation error mean: {mean_f_err}, std error: {std_f_err}==')
+
+
+def eval_burgers(model,
+                 dataloader,
+                 config,
+                 device,
+                 use_tqdm=True):
+    model.eval()
+    myloss = LpLoss(size_average=True)
+    if use_tqdm:
+        pbar = tqdm(dataloader, dynamic_ncols=True, smoothing=0.05)
+    else:
+        pbar = dataloader
+
+    test_err = []
+    f_err = []
+
+    for x, y in pbar:
+        x, y = x.to(device), y.to(device)
+        out = model(x).reshape(y.shape)
+        data_loss = myloss(out, y)
+
+        loss_u, f_loss = PINO_loss(out, x[:, 0, :, 0])
+        test_err.append(data_loss.item())
+        f_err.append(f_loss.item())
+
+    mean_f_err = np.mean(f_err)
+    std_f_err = np.std(f_err, ddof=1) / np.sqrt(len(f_err))
+
+    mean_err = np.mean(test_err)
+    std_err = np.std(test_err, ddof=1) / np.sqrt(len(test_err))
+
+    print(f'==Averaged relative L2 error mean: {mean_err}, std error: {std_err}==\n'
+          f'==Averaged equation error mean: {mean_f_err}, std error: {std_f_err}==')
+
