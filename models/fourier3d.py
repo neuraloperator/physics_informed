b91af121a106d442a5390342a71f7743d5a1f67c
devzhk <hzzheng@caltech.edu>
2021-11-12 17:22:52 -0800

    Roll back grad checkpointing

--- models/fourier3d.py
+++ models/fourier3d.py
@@ -1,17 +1,12 @@
 import torch
 import torch.nn as nn
-import torch.utils.checkpoint as checkpoint
+import torch.nn.functional as F
 
-from .basics import FourierBlock
+from .basics import SpectralConv3d
 
 
 class FNN3d(nn.Module):
-    def __init__(self, modes1, modes2, modes3,
-                 width=16, fc_dim=128,
-                 layers=None,
-                 in_dim=4, out_dim=1,
-                 activation='tanh',
-                 use_checkpoint=False):
+    def __init__(self, modes1, modes2, modes3, width=16, fc_dim=128, layers=None, in_dim=4, out_dim=1):
         '''
         Args:
             modes1: list of int, first dimension maximal modes for each layer
@@ -20,22 +15,11 @@ class FNN3d(nn.Module):
             layers: list of int, channels for each layer
             in_dim: int, input dimension
             out_dim: int, output dimension
-            activation: str or list, name of activation functions, options: tanh, gelu, none;
-                        If none, no activation function will be applied.
-            use_checkpoint: If True, use gradient checkpointing to trade compute for memory
         '''
         super(FNN3d, self).__init__()
         self.modes1 = modes1
         self.modes2 = modes2
         self.modes3 = modes3
-        if isinstance(activation, str):
-            self.activation = [activation] * len(self.modes1)
-        elif isinstance(activation, list):
-            self.activation = activation
-        else:
-            raise ValueError('activations are not supported')
-
-        self.use_checkpoint = use_checkpoint
 
         if layers is None:
             self.layers = [width] * 4
@@ -43,19 +27,16 @@ class FNN3d(nn.Module):
             self.layers = layers
         self.fc0 = nn.Linear(in_dim, layers[0])
 
-        self.blocks = nn.ModuleList(
-            [
-                FourierBlock(in_size, out_size, mode1_num, mode2_num, mode3_num, activation=act)
-                for in_size, out_size, mode1_num, mode2_num, mode3_num, act
-                in zip(self.layers, self.layers[1:], self.modes1, self.modes2, self.modes3, self.activation)
-            ]
-        )
+        self.sp_convs = nn.ModuleList([SpectralConv3d(
+            in_size, out_size, mode1_num, mode2_num, mode3_num)
+            for in_size, out_size, mode1_num, mode2_num, mode3_num
+            in zip(self.layers, self.layers[1:], self.modes1, self.modes2, self.modes3)])
 
-        self.project = nn.Sequential(
-            nn.Linear(layers[-1], fc_dim),
-            nn.Tanh(),
-            nn.Linear(fc_dim, out_dim)
-        )
+        self.ws = nn.ModuleList([nn.Conv1d(in_size, out_size, 1)
+                                 for in_size, out_size in zip(self.layers, self.layers[1:])])
+
+        self.fc1 = nn.Linear(layers[-1], fc_dim)
+        self.fc2 = nn.Linear(fc_dim, out_dim)
 
     def forward(self, x):
         '''
@@ -66,20 +47,21 @@ class FNN3d(nn.Module):
             u: (batchsize, x_grid, y_grid, t_grid, 1)
 
         '''
+        length = len(self.ws)
+        batchsize = x.shape[0]
+        size_x, size_y, size_z = x.shape[1], x.shape[2], x.shape[3]
+
         x = self.fc0(x)
         x = x.permute(0, 4, 1, 2, 3)
-        # if self.use_checkpoint:
-        #     x = checkpoint.checkpoint(self.blocks, x)
-        # else:
-        #     x = self.blocks(x)
-        for block in self.blocks:
-            if self.use_checkpoint:
-                x = checkpoint.checkpoint(block, x)
-            else:
-                x = block(x)
+
+        for i, (speconv, w) in enumerate(zip(self.sp_convs, self.ws)):
+            x1 = speconv(x)
+            x2 = w(x.view(batchsize, self.layers[i], -1)).view(batchsize, self.layers[i+1], size_x, size_y, size_z)
+            x = x1 + x2
+            if i != length - 1:
+                x = torch.tanh(x)
         x = x.permute(0, 2, 3, 4, 1)
-        if self.use_checkpoint:
-            x=  checkpoint.checkpoint(self.project, x)
-        else:
-            x = self.project(x)
+        x = self.fc1(x)
+        x = torch.tanh(x)
+        x = self.fc2(x)
         return x
