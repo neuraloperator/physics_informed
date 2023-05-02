97ef975693b443abe1d9ef1af789790d8978f99f
Dor Shacham <dor.shacham@gmail.com>
2023-05-02 15:18:53 +0300

    Co-authored-by: NatPath <NatPath@users.noreply.github.com>

--- train_utils/datasets.py
+++ train_utils/datasets.py
@@ -113,6 +113,41 @@ class BurgersLoader(object):
             loader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=False)
         return loader
 
+class SPDCInvLoader(object):
+    def __init__(self, datapath, nx=2 ** 10, nt=100, sub=8, sub_t=1, new=False):
+        dataloader = MatReader(datapath)
+        self.sub = sub
+        self.sub_t = sub_t
+        self.s = nx // sub
+        self.T = nt // sub_t
+        self.new = new
+        if new:
+            self.T += 1
+        self.x_data = dataloader.read_field('input')[:, ::sub]
+        self.y_data = dataloader.read_field('output')[:, ::sub_t, ::sub]
+        self.v = dataloader.read_field('visc').item()
+
+    def make_loader(self, n_sample, batch_size, start=0, train=True):
+        Xs = self.x_data[start:start + n_sample]
+        ys = self.y_data[start:start + n_sample]
+
+        if self.new:
+            gridx = torch.tensor(np.linspace(0, 1, self.s + 1)[:-1], dtype=torch.float)
+            gridt = torch.tensor(np.linspace(0, 1, self.T), dtype=torch.float)
+        else:
+            gridx = torch.tensor(np.linspace(0, 1, self.s), dtype=torch.float)
+            gridt = torch.tensor(np.linspace(0, 1, self.T + 1)[1:], dtype=torch.float)
+        gridx = gridx.reshape(1, 1, self.s)
+        gridt = gridt.reshape(1, self.T, 1)
+
+        Xs = Xs.reshape(n_sample, 1, self.s).repeat([1, self.T, 1])
+        Xs = torch.stack([Xs, gridx.repeat([n_sample, self.T, 1]), gridt.repeat([n_sample, 1, self.s])], dim=3)
+        dataset = torch.utils.data.TensorDataset(Xs, ys)
+        if train:
+            loader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=True)
+        else:
+            loader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=False)
+        return loader
 
 class NSLoader(object):
     def __init__(self, datapath1,
